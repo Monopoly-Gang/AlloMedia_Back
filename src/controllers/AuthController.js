7f1bec1fe8417168c7f57fe92c4f36d7fcb10315
Anass-Dr <anasdrissi77@gmail.com>
2024-10-20 15:05:30 +0100

    AB-92 Add email verification

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -23,6 +23,31 @@ class AuthController {
             res.status(500).json({error: error.message});
         }
     }
+
+    async sendEmailVerification(req, res) {
+        try {
+            const user = await User.findOne({email: req.body.email});
+            if (!user) return res.status(404).json({message: 'User not found'});
+            const emailSent = await sendEmailVerification(user._id, user.email);
+            if (emailSent.error) return res.status(500).json({message: emailSent.error});
+            res.status(200).json({message: 'Email verification sent successfully'});
+        } catch (error) {
+            res.status(500).json({error: error.message});
+        }
+    }
+
+    async verifyEmail (req, res) {
+        try {
+            const decoded = req.decoded;
+
+            // Update user status
+            const user = await User.findByIdAndUpdate(decoded.id, {isVerified: true}, {new: true});
+            if (!user) return res.status(404).json({message: 'User not found'});
+            res.status(200).json({message: 'Email verified successfully'});
+        } catch (error) {
+            res.status(500).json({error: error.message});
+        }
+    }
 }
 
 module.exports = new AuthController();
