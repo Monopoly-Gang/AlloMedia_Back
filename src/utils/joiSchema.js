fcc3f9016481a91826de1ec65931e28195802800
Anass-Dr <anasdrissi77@gmail.com>
2024-10-20 15:24:53 +0100

    AB-95 Add inputs validation messages for (required and empty)

--- src/utils/joiSchema.js
+++ src/utils/joiSchema.js
@@ -17,14 +17,20 @@ const schemas = {
         .string()
         .email()
         .required()
-        .messages({'string.pattern.base': 'Email must be a valid email', 'string.empty': 'Email is not allowed to be empty'}),
+        .messages({
+            'string.pattern.base': 'Email must be a valid email',
+            'string.empty': 'Email is not allowed to be empty',
+            'any.required': 'Email is required',
+            'string.email': 'Email must be a valid email'
+        }),
     password: Joi
         .string()
         .pattern(new RegExp('^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$'))
         .required()
         .messages({
             'string.pattern.base': 'Password must be at least 8 characters long, include one uppercase letter, one lowercase letter, and one number',
-            'string.empty': 'Password is not allowed to be empty'
+            'string.empty': 'Password is not allowed to be empty',
+            'any.required': 'Password is required'
         }),
     phoneNumber: Joi
         .string()
@@ -32,8 +38,13 @@ const schemas = {
         .required()
         .messages({
             'string.pattern.base': 'Phone number must be in international format',
+            'string.empty': 'Phone number is not allowed to be empty',
+            'any.required': 'Phone number is required'
         }),
-    address: Joi.string().required(),
+    address: Joi.string().required().messages({
+        'string.empty': 'Address is not allowed to be empty',
+        'any.required': 'Address is required'
+    }),
 };
 
 module.exports = (...fields) => {
