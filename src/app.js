ba57f7a34db21a5c8822feb1faeebff693ce4859
youness <younesserbai@gmail.com>
2024-10-25 10:24:47 +0200

    chore: remove uploads folder from gitignore to track uploaded files

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const routes = require("./routes");
-
+const path = require('path')
 const app = express();
 const corsOptions = {
     origin: process.env.FRONT_APP_HOST,
@@ -19,6 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static("../public"));
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
 app.use("/api", routes);
 
 app.use("/api",routes );
