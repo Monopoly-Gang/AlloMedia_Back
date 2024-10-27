0cb61107ccae81db4b76bd599b29697b309e7d3d
Anass-Dr <anasdrissi77@gmail.com>
2024-10-27 17:52:11 +0100

    Remove restaurant_id from menuItem model

--- src/models/MenuItem.js
+++ src/models/MenuItem.js
@@ -5,7 +5,6 @@ const menuItemSchema = new mongoose.Schema({
     description: String,
     price: { type: Number, required: true },
     image: String,
-    restaurant: { type: mongoose.Schema.Types.ObjectId, ref: 'Restaurant', required: true },
 }, { timestamps: true });
 
 module.exports = mongoose.model('MenuItem', menuItemSchema);
