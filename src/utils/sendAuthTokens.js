5d9a61f713411ff4665bc2123a71fcfd7440b5ec
Anass-Dr <anasdrissi77@gmail.com>
2024-10-28 19:13:38 +0100

    Send User id, fullName, role and email in response after logging successfully

--- src/utils/sendAuthTokens.js
+++ src/utils/sendAuthTokens.js
@@ -1,13 +1,27 @@
 const jwtService = require("../services/jwtService");
 
 module.exports = async function sendAuthTokens(res, userData) {
-    const accessToken = jwtService.generateToken(userData, 30 * 60);
-    const refreshToken = jwtService.generateToken(userData, 7 * 24 * 60 * 60);
-    res.cookie("refreshToken", refreshToken, {
-        httpOnly: true,
-        secure: false,
-        sameSite: 'strict',
-        maxAge: 7 * 24 * 60 * 60 * 1000,
-    });
-    res.status(200).json({ accessToken });
+  const accessToken = jwtService.generateToken(
+    { id: userData._id, role: userData.role },
+    30 * 60
+  );
+  const refreshToken = jwtService.generateToken(
+    { id: userData._id, role: userData.role },
+    7 * 24 * 60 * 60
+  );
+  res.cookie("refreshToken", refreshToken, {
+    httpOnly: true,
+    secure: false,
+    sameSite: "strict",
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+  });
+  res.status(200).json({
+    accessToken,
+    user: {
+      id: userData._id,
+      fullName: userData.fullName,
+      email: userData.email,
+      role: userData.role,
+    },
+  });
 };
